cf589fa00bfcb23362a5fae0dcd6c67150acc27c
EdwardRadzh <69647887+EdwardRadzh@users.noreply.github.com>
2021-12-13 17:47:40 +0300

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import { Route } from 'react-router-dom';
 import Navigation from "../Navigation/Navigation";
 import Logo from "../Logo/Logo";
 
-function Header({ isLoggedIn }) {
+function Header({ loggedIn }) {
     const endpoints = ['/movies', '/saved-movies', '/profile', '/'];
     return (
         <Route exact path={endpoints}>
           <header className='header'>
             <Logo />
-            <Navigation isLoggedIn={isLoggedIn}/>
+            <Navigation loggedIn={loggedIn}/>
           </header>
         </Route>
     );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,50 +3,52 @@ import React from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
 import BurgerNav from '../BurgerNav/BurgerNav';
 
-function Navigation({ isLoggedIn }) {
+function Navigation({ loggedIn }) {
 
     const location = useLocation();
     const [isMenuActive, setIsMenuActive] = React.useState(false);
 
     return (
-        <nav className={`menu ${location.pathname === "/" ? "" : 'menu__account menu__main-burger'}`}>
-            {location.pathname === "/" && !isLoggedIn ? 
+        <nav className={`menu ${location.pathname === '/' ? "" : 'menu__account menu__main-burger'}`}>
+            {loggedIn  ? 
             (
                 <>
-                    <Link to='/signup' className='menu__link page__link'>Регистрация</Link>
-                    <Link to='/signin' className='menu__link menu__link_type_signin page__link'>Войти</Link>
-                </>
+                <div className="menu__main">
+                <Link to='/movies'  className='memu__link menu__film-link page__link'
+                >
+                Фильмы
+                </Link>
+                <Link to='/saved-movies' className='menu__link menu__film-link page__link'
+                >
+                Сохраненные фильмы
+                </Link>
+                <div className="menu__account">
+                    <Link to='/profile' className='menu__link menu__link_type_profile page__link '
+                    >
+                    Аккаунт
+                    </Link>
+                    <Link to='/profile' className='menu__pic page__link' alt='иконка аккаунта'></Link>
+                </div>
+            </div>
+            <div className="menu__main-burger">
+                <button className="menu__burger-button" 
+                        onClick={() => setIsMenuActive(!isMenuActive)}
+                />
+                <BurgerNav
+                    active={isMenuActive}
+                    setActive={setIsMenuActive} />
+            </div>
+            </>
+
             ) : (
-                <>
-                    <div className="menu__main">
-                    <Link to='/movies'  className='memu__link menu__film-link page__link'
-                    >
-                    Фильмы
-                    </Link>
-                    <Link to='/saved-movies' className='menu__link menu__film-link page__link'
-                    >
-                    Сохраненные фильмы
-                    </Link>
-                    <div className="menu__account">
-                        <Link to='/profile' className='menu__link menu__link_type_profile page__link '
-                        >
-                        Аккаунт
-                        </Link>
-                        <Link to='/profile' className='menu__pic page__link' alt='иконка аккаунта'></Link>
-                    </div>
-                </div>
-                <div className="menu__main-burger">
-                    <button className="menu__burger-button" 
-                            onClick={() => setIsMenuActive(!isMenuActive)}
-                    />
-                    <BurgerNav
-                        active={isMenuActive}
-                        setActive={setIsMenuActive} />
-                </div>
-                </>
+            <>
+                <Link to='/signup' className='menu__link page__link'>Регистрация</Link>
+                <Link to='/signin' className='menu__link menu__link_type_signin page__link'>Войти</Link>
+            </>
             )}
         </nav>
     );
 };
 
 export default Navigation;
+
